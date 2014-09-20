eac1d698bc3864168819f7600f6f636e63b3bfe3
Bjorn Tipling <bjorn@ambientchill.com>
2014-09-19 22:48:58 -0700

    Set up a build system, using args.

--- bin/cli.js
+++ bin/cli.js
@@ -1,16 +1,24 @@
+#!/usr/bin/env node
 "use strict";
-var djanglets = require("./djanglets").parser,
-    keys, index, key,
-    contents,
-    parsed,
-    filename = "foo.html",
-    yy = {},
-    ast = require("./ast"),
+var djanglets = require("../lib/djanglets").parser,
+    argv = require('yargs'),
+    keys, index, key, contents, parsed, filename, yy = {},
+    ast = require("../lib/djanglets/ast"),
     util = require("util"),
     fs = require("fs"),
     result, fileContent,
     outFilename = "build.templets.js";
 
+
+argv = argv.usage('Compile a djanglate template.\nUsage: $0 template')
+ .example('$0 -o filename', 'Output file name.')
+ .demand(1)
+ .alias('o', 'output')
+ .describe('o', 'Output file name')
+ .argv;
+
+outFilename = argv.output || outFilename;
+filename = argv._[0]
 
 contents = fs.readFileSync(filename, "utf-8");
 keys = Object.keys(ast);
@@ -25,6 +33,6 @@ fs.writeFile(outFilename, fileContent, function(err) {
   if(err) {
     console.log("Error saving template", err);
   } else {
-    console.log("Template saved to ", outFilename);
+    console.log("Template saved to", outFilename);
   }
 }); 

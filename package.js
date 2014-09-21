7b929ce392f0c8d66dda9f030a3fd8501a6eaa01
Bjorn Tipling <bjorn@ambientchill.com>
2014-09-21 12:51:50 -0700

    Fix &amp; error, add meteor packaging.

---
+++ package.js
@@ -0,0 +1,25 @@
+Package.describe({
+  summary: "Djanglets templates for meteor.",
+  version: "0.1.0",
+  git: "https://github.com/btipling/djanglets",
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@0.9.2.2');
+  api.addFiles('lib/meteor/djanglets.js');
+});
+
+Package._transitional_registerBuildPlugin({
+    name: "compileDjanglets",
+    use: [],
+    sources: [
+      'lib/compile-djanglets.js'
+    ],
+    npmDependencies: {}
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('bjorn:djanglets');
+  api.addFiles('lib/meteor/djanglets-tests.js');
+});

286194128135dca41e59baa36ca1b3e671e80d77
Bjorn Tipling <bjorn@ambientchill.com>
2014-09-24 22:01:36 -0700

    Rendering ifs now in normal nonlive render.

--- test/dev.js
+++ test/dev.js
@@ -1,5 +1,11 @@
 var template, html;
 template = djanglets[djanglets.templates[0]];
-html = template.toString({foovar: "<p>Whatup</p>"});
+console.log("before");
+html = template.toString({
+  foovar: "<p>Whatup</p>",
+  fooifcheck: false,
+  fooelifcheck: true,
+});
+console.log("after", html);
 document.getElementById("result").innerHTML = html;
 

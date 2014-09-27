824fb8a9ac4d47afdfd5df23e9829359e66d7ca4
Bjorn Tipling <bjorn@ambientchill.com>
2014-09-27 00:32:31 -0700

    Rendering regular fors!

--- test/dev.js
+++ test/dev.js
@@ -5,6 +5,15 @@ html = template.toString({
   foovar: "<p>Whatup</p>",
   fooifcheck: false,
   fooelifcheck: true,
+  foos: {
+    dog: "woof",
+    cows: "moo",
+  },
+  foosR: [
+    "red",
+    "white",
+    "blue",
+  ],
 });
 console.log("after", html);
 document.getElementById("result").innerHTML = html;

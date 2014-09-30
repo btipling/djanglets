7500639c27c262e75869a441849dad21e83ffbee
Bjorn Tipling <bjorn@ambientchill.com>
2014-09-30 08:25:20 -0700

    Added test for flattenPreREnder.

--- test/utils.js
+++ test/utils.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var utils = require("../lib/djanglets/utils");
+
 module.exports = {
   setUp: function (callback) {
     callback();
@@ -8,7 +10,33 @@ module.exports = {
     callback();
   },
   testFlattenPrerender: function (test) {
-    test.equals(true, true);
+    var pieces, expectedStr1, expectedStr2, expectedStr3, expectedLength, results = [];
+    expectedStr1 = "The quick red fox jumped over the lazy brown dog.";
+    expectedStr2 = " foo  foo";
+    expectedStr3 = " foobar";
+    expectedLength = 5;
+
+    pieces = [
+      "The quick",
+      [
+        " ",
+        "red ",
+        [
+          "fox jumped over", " ",
+        ],
+        "the lazy brown",
+      ],
+      " dog.",
+      {},
+      " foo ",
+      [ " ", "foo", {}, " foo",],
+      "bar",
+    ];
+    utils.flattenPreRender(results, pieces);
+    test.equals(results.length, expectedLength, "Expected array to have been flattened.");
+    test.equals(results[0], expectedStr1, "Expected strings to be concatonated.");
+    test.equals(results[2], expectedStr2, "Expected subsequent strings to be concatonated");
+    test.equals(results[4], expectedStr3, "Expected last string to be concatonated.");
     test.done();
   },
 };

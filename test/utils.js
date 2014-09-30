18b12d4a7400c513503652eff7b0c5bc66474332
Bjorn Tipling <bjorn@ambientchill.com>
2014-09-30 10:26:57 -0700

    Add more util tests, fix one bug test found!

--- test/utils.js
+++ test/utils.js
@@ -1,6 +1,7 @@
 "use strict";
 
-var utils = require("../lib/djanglets/utils");
+var utils = require("../lib/djanglets/utils"),
+    meteorVariable = require("../lib/meteor/variable");
 
 module.exports = {
   setUp: function (callback) {
@@ -37,6 +38,88 @@ module.exports = {
     test.equals(results[0], expectedStr1, "Expected strings to be concatonated.");
     test.equals(results[2], expectedStr2, "Expected subsequent strings to be concatonated");
     test.equals(results[4], expectedStr3, "Expected last string to be concatonated.");
+    test.done();
+  },
+  testBasicVariableLookup: function (test) {
+    var result, expected = "bar", data = {foo: "bar"};
+    result = utils.getVariable("foo", data);
+    test.equals(result, expected, "Expected to get a variable from an object.");
+    test.done();
+  },
+  testNestedVariableLookup: function (test) {
+    var result, expected = "bar", data = {foo: {bar: "bar"}};
+    result = utils.getVariable("foo.bar", data);
+    test.equals(result, expected, "Expected to get nested variable lookup to work.");
+    test.done();
+  },
+  testContextLookup: function (test) {
+    var result, expected = "bar", data = {a: "b", __context__: {foo: "bar"}};
+    result = utils.getVariable("foo", data);
+    test.equals(result, expected, "Expected to have contexts checked.");
+    test.done();
+  },
+  testArrayLookup: function (test) {
+    var result, expected1 = "bar", expected2 = "foo", data = [{foo: "bar"}, {bar: "foo"}];
+    result = utils.getVariable("foo", data);
+    test.equals(result, expected1, "Expected foo variable look up with multiple datas.");
+    result = utils.getVariable("bar", data);
+    test.equals(result, expected2, "Expected bar variable look up with multiple datas.");
+    test.done();
+  },
+  testNestedContextArrayLookup: function (test) {
+    var result, expected, data;
+    data = [
+      {
+        a: "b",
+        c: "d",
+        __context__: {
+          aa: "bb",
+          cc: "dd",
+          __context__: {
+            foo: "bar",
+            foobar: "foobar",
+          }
+        }
+      },
+      {
+        dog: "woof",
+        cat: "nope",
+        __context__: {
+          test: "testing",
+          foo: "notbar",
+          cat: "meow",
+        },
+      },
+    ];
+    expected = "bar";
+    result = utils.getVariable("foo", data);
+    test.equals(result, expected, "Expected first item in array to be examined first");
+    expected = "testing";
+    result = utils.getVariable("__context__.test", data);
+    test.equals(result, expected, "Expected second data in array to find lookup.");
+    expected = "meow";
+    result = utils.getVariable("__context__.cat", data);
+    test.equals(result, expected, "Expected first cat to be ignored.");
+    test.done();
+  },
+  testLiveVariableObjectLookup: function (test) {
+    var variable, result, expected, data;
+    variable = meteorVariable.createVariable("foo");
+    variable.value = "bar";
+    expected = "bar";
+    data = {a: "b", foo: variable};
+    result = utils.getVariable("foo", data);
+    test.equals(result, expected, "Expected a live variable lookup to work.");
+    test.done();
+  },
+  testLiveVariableLookupInArray: function (test) {
+    var variable, result, expected, data;
+    variable = meteorVariable.createVariable("foo");
+    variable.value = "bar";
+    expected = "bar";
+    data = [{a: "b"}, {foo: variable}];
+    result = utils.getVariable("foo", data);
+    test.equals(result, expected, "Expected a live variable lookup to work.");
     test.done();
   },
 };

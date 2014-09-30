bc4385be785afd3463b5110c7ac37be27cf85d0f
Bjorn Tipling <bjorn@ambientchill.com>
2014-09-29 17:26:08 -0700

    Stubbed out first test file.

---
+++ test/utils.js
@@ -0,0 +1,14 @@
+"use strict";
+
+module.exports = {
+  setUp: function (callback) {
+    callback();
+  },
+  tearDown: function (callback) {
+    callback();
+  },
+  testFlattenPrerender: function (test) {
+    test.equals(true, true);
+    test.done();
+  },
+};

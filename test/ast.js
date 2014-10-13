0bdad8afc775c16c8925470c179e31b6471cf29a
Bjorn Tipling <bjorn@ambientchill.com>
2014-10-12 22:28:39 -0700

    Added more visitor tests for open element.

--- test/ast.js
+++ test/ast.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var ast, visitor,
+   template = require("../lib/djanglets/template"),
    djAst = require("../lib/djanglets/ast");
 
 module.exports = {
@@ -38,17 +39,35 @@ module.exports = {
     test.done();
   },
   testVisitOpenElementTemplateAddsToAst: function (test) {
+    var el;
     visitor = djAst.visitor;
     ast = djAst.createAst();
+    test.equal(ast.state.ast, null, "Ast starts as null");
+    el = visitor.visitOpenElement(ast, template.TEMPLATE_TYPE);
+    test.equal(el.type, template.TEMPLATE_TYPE, "Created element should be template type.");
+    test.equal(ast.state.ast[0], el, "Template element added to ast.");
     test.done();
   },
   testVisitOpenElementNodeAddedToChildren: function (test) {
+    var node, element;
+    node = ast.getCurrentNode();
+    element = visitor.visitOpenElement(ast, "div");
+    test.equal(node.children[0], element, "New element should have been added to children.");
     test.done();
   },
   testVisitOpenElementNodeAddedToNodeStack: function (test) {
+    var element;
+    element = visitor.visitOpenElement(ast, "div");
+    test.equal(ast.getCurrentNode(), element, "New element should be end of stack.");
     test.done();
   },
   testVisitOpenElementNodeEndsAttributes: function (test) {
+    var ran = false;
+    visitor.endAttributes = function () {
+      ran = true;
+    }
+    visitor.visitOpenElement(ast, "div");
+    test.ok(ran, "Should have ended attributes.");
     test.done();
   },
 }

2ad1811c88bc0b41ba9f912d7f8d1fff8ded71b8
Bjorn Tipling <bjorn@ambientchill.com>
2014-10-13 16:13:59 -0700

    Added ast tests. #25

--- test/ast.js
+++ test/ast.js
@@ -16,23 +16,39 @@ module.exports = {
     test.done();
   },
   testAstAddNode: function (test) {
-    test.ok(false);
+    test.equal(ast.state.nodeStack[0], undefined, "Should not have any nodes.");
+    ast.addNodeToStack("foo");
+    test.equal(ast.state.nodeStack[0], "foo", "Should have added node to stack.");
     test.done();
   },
   testAstGetCurrentNode: function (test) {
-    test.ok(false);
+    test.ok(!ast.getCurrentNode(), "Should not have any nodes yet.");
+    ast.addNodeToStack("foo");
+    test.equal(ast.getCurrentNode(), "foo", "Should have gotten current node.");
     test.done();
   },
   testPopNodeStack: function (test) {
-    test.ok(false);
+    ast.addNodeToStack("foo");
+    test.equal(ast.getCurrentNode(), "foo", "Should have gotten current node.");
+    ast.popNodeStack()
+    test.ok(!ast.getCurrentNode(), "Should not have any any more nodes.");
     test.done();
   },
   testValueOf: function (test) {
-    test.ok(false);
+    var expect = [{"foo": "bar"}];
+    ast.state.ast = expect;
+    test.deepEqual(expect, ast.valueOf());
     test.done();
   },
   testState: function (test) {
-    test.ok(false);
+    console.log(ast.state);
+    ast.state.ast = ["foo"];
+    test.deepEqual(ast.state, {
+      currentText: '',
+      nodeStack: [],
+      currentAttributes: [],
+      ast: ["foo"],
+    });
     test.done();
   },
 };
